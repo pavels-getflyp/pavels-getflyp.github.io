e31b29cb98f9bfca643bb36382ddaabb853c029b
pavel <pavels1987@gmail.com>
2018-05-06 22:58:01 +0300

    adding my files to my repository

---
+++ mdn/first_steps/scripts/main.js
@@ -0,0 +1,32 @@
+var myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello world!';
+
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/google.jpg') {
+        myImage.setAttribute ('src','images/apple.jpg');
+    } else {
+        myImage.setAttribute ('src','images/google.jpg');
+    }
+}
+
+var myButton = document.querySelector('button');
+
+function setUserName() {
+    var myName = prompt('Please enter your name.');
+    localStorage.setItem('name', myName);
+    myHeading.innerHTML  = 'Mozilla is cool, ' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    var storedName = localStorage.getItem('name');
+    myHeading.innerHTML  = 'Mozilla is cool, ' + storedName;
+}
+
+myButton.onclick = function() {
+    setUserName();
+}
